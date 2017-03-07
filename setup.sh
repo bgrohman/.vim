438956437f17f98ea5b34f270dbde7cb0df00ede
bgrohman <bgrohman@nice.com>
2017-03-07 14:58:40 -0600

    instructions for setting up vimfiles link in windows

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ git submodule init
 git submodule update
 mkdir ~/.vim-backup
 ln -s "$cwd" ~/.vim
+
+# WINDOWS
+# Create link for vimfiles directory.
+# mklink /D C:\Users\<user>\vimfiles <path_to_.vim_directory>
